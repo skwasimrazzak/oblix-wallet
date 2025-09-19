561e7f64d7b49300e1a84ec9ce12e174f088d2cf
Wasim Razzak <wrazzak24@gmail.com>
2025-09-19 22:31:43 +0530

    Initial Build

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(OblixWallet());
+}
+
+class OblixWallet extends StatelessWidget {
+  const OblixWallet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Oblix Wallet',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Oblix Wallet'), centerTitle: true),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  //Action
+                },
+                child: Text('Generate Wallet'),
+              ),
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
